0c6362568c4d9bb92e6415c21b05d090c9342d58
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-02-09 13:55:24 +0000

    Fix Mentat Scripts: flutter format --set-exit-if-c...
    Error output:
    
    ==============================
    Full log:[here ❌](https://mentat.ai/log/2c0bb907-aeb8-49d3-bbed-32909716d886)

--- .mentat/precommit.sh
+++ .mentat/precommit.sh
@@ -1,3 +1,4 @@
+export PATH=$PATH:/opt/flutter/bin
 flutter format --set-exit-if-changed .
 flutter analyze
 flutter test
--- .mentat/setup.sh
+++ .mentat/setup.sh
@@ -1,6 +1,5 @@
 apt-get update
 apt-get install -y unzip
 export PATH=$PATH:/opt/flutter/bin
-yes | flutter doctor --android-licenses || true
 flutter pub get
-flutter build
+flutter build web
