8b81d584d430773d6cc0de8d6547d12c19892deb
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-02-09 15:04:33 +0000

    Fix Mentat Scripts: ./flutter/bin/dart format --se...
    Error output:
    =====10 Lines Omitted=====
    
    ==============================
    Full log:[here ❌](https://mentat.ai/log/91bbb771-b5be-4a16-9ef3-39a0099693e0)

--- .mentat/precommit.sh
+++ .mentat/precommit.sh
@@ -1,3 +1,3 @@
-./flutter/bin/dart format --set-exit-if-changed=false .
-./flutter/bin/flutter analyze --no-fatal-infos --no-fatal-warnings || true
+./flutter/bin/dart format . || true
+./flutter/bin/flutter analyze || true
 ./flutter/bin/flutter test || true
