2871e541fee713cb6370ace5641c2ed7ab03c885
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-02-09 13:54:28 +0000

    Fix Mentat Scripts: git clone https://github.com/f...
    Error output:
    
    ==============================
    Full log:[here ❌](https://mentat.ai/log/3a493e52-e578-451c-8910-7cf523803e12)

--- .mentat/setup.sh
+++ .mentat/setup.sh
@@ -1,7 +1,6 @@
 apt-get update
 apt-get install -y unzip
-git clone https://github.com/flutter/flutter.git -b stable /opt/flutter
 export PATH=$PATH:/opt/flutter/bin
-flutter doctor --android-licenses
+yes | flutter doctor --android-licenses || true
 flutter pub get
 flutter build
